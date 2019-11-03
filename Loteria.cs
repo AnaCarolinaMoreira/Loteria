86908fcd194c327c3ce8b708797e4dc59a931d97
Nome Sobrenome <seu_email@email.com>
2019-11-02 22:50:08 -0300

    first commit

---
+++ Loteria.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Loteria
+    {
+        private int qtdDezenas;
+        private int totalJgos;
+        private int[][] jogos;
+        private int[] resultado;
+        Random r = new Random();
+
+        public Loteria(int dezenas, int tjogos)
+        {
+            QtdDezenas = dezenas;
+            TotalJgos = tjogos;
+        }
+
+        public int QtdDezenas
+        {
+            get => qtdDezenas;
+            set
+            {
+                if (value >= 6 && value <= 10)
+                    qtdDezenas = value;
+                else
+                    throw new InvalidOperationException();
+            }
+        }
+        public int TotalJgos { get => totalJgos; set => totalJgos = value; }
+        public int[][] Jogos { get => jogos; set => jogos = value; }
+        public int[] Resultado { get => resultado; set => resultado = value; }
+
+        private int[] sortearNumeros()
+        {
+
+
+            int[] numeros = new int[QtdDezenas];
+            for (int i = 0; i < QtdDezenas; i++)
+            {
+                int n = r.Next(1, 61);
+                while (numeros.Contains(n))
+                    n = r.Next(1, 61);
+
+                numeros[i] = n;
+            }
+
+            return numeros;
+        }
+
+        public void SortearJogos()
+        {
+            jogos = new int[TotalJgos][];
+            for (int i = 0; i < TotalJgos; i++)
+            {
+                jogos[i] = sortearNumeros();
+            }
+        }
+
+        public void ExecutarSorteio()
+        {
+            Random r = new Random();
+
+            int[] numeros = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                int n = r.Next(1, 61);
+                while (numeros.Contains(n))
+                    n = r.Next(1, 61);
+
+                numeros[i] = n;
+            }
+
+            Resultado = numeros.OrderBy(i => i).ToArray();
+        }
+
+        public int[,] ApurarResultado()
+        {
+            int[,] result = new int[totalJgos, QtdDezenas + 1];
+
+            int x = 0;
+
+            for (int i = 0; i < totalJgos; i++)
+            {
+
+                for (int j = 0; j < qtdDezenas; j++)
+                {
+                    if (jogos[i].Contains(Resultado[j]))
+                        result[i, QtdDezenas]++;
+
+                    result[i, j] = jogos[i][j];
+
+                }
+
+
+            }
+
+
+
+            return result;
+        } 
+
+        }
+}
---
+++ Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+            Loteria lot = new Loteria(6, 2);
+            lot.SortearJogos();
+            lot.ExecutarSorteio();
+
+            int[,] result = lot.ApurarResultado();
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+
+                  
+                    await context.Response.WriteAsync("\t" + result[i, j] );       
+                    await context.Response.WriteAsync("\n");
+                 //  await context.Response.WriteAsync("{0:00}\t"+result[i, j]);
+                
+
+                }
+
+
+               // await context.Response.WriteAsync("Hello World!"+ numero.ToString());
+            });
+        }
+
+    }
+}
